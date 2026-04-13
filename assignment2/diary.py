b8e332c4b0c57815f68934d77c40b0dc8a5de3ec
jf2024 <josexfuentes27@gmail.com>
2026-04-12 17:19:30 -0700

    first task completed

---
+++ assignment2/diary.py
@@ -0,0 +1,28 @@
+# task1 - diary 
+
+import traceback
+
+try: 
+    with open('diary.txt', 'a') as file:
+        first_input = input("What happened today? ")
+        file.write(first_input + "\n")
+
+        while True: 
+           inputs = input("What else?: ")
+           file.write(inputs + "\n")
+
+           if inputs.lower() == "done for now":
+              break
+
+except Exception as e:
+   trace_back = traceback.extract_tb(e.__traceback__)
+   stack_trace = list()
+   for trace in trace_back:
+      stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+   print(f"Exception type: {type(e).__name__}")
+   message = str(e)
+   if message:
+      print(f"Exception message: {message}")
+   print(f"Stack trace: {stack_trace}")
+
+
